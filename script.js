8de9343a9f8bb8b97e266b6fc33c278b03971f2a
Devin <devin.everitt96@gmail.com>
2023-12-18 17:35:01 -0500

    added functionality to no button

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-var questionEl = document.querySelector(".question")
+var questionEl = document.querySelector('.question');
+var sayYes = document.querySelector('#yes');
+var sayNo = document.querySelector('#no');
 
-console.log(questionEl)
+console.log(questionEl);
+console.log(sayYes);
+console.log(sayNo);
+
+
+sayYes.addEventListener("click", funciton() {
+
+
+
+
+    
+} )
+
+
+sayNo.addEventListener("click", function() {
+    alert("then why are you here? press yes.")
+
+});
